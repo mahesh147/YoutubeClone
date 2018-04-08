f152d17f59fbc81b4ff9d33601870db2e5a462e0
Mahesh Chandran <maheshchandran147@gmail.com>
2018-04-08 19:29:19 +0530

    Final commit

---
+++ src/components/video_list_item.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const VideoListItem = ({video, onVideoSelect}) => {
+const imageURL = video.snippet.thumbnails.default.url;
+return (
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
+        <div className="video-list media">
+            <div className="media-left">
+                <img className="media-object" src={imageURL} />
+            </div>
+            <div className="media-body">
+                <div className="media-heading">{video.snippet.title}</div>
+            </div>
+        </div>
+    </li>
+    );
+};
+
+export default VideoListItem;
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import YTsearch from 'youtube-api-search';
+import _ from 'lodash';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyDoN_H9M-AjT29_eHAl_yXiqBbvgrfk2sI';
+
+class App extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null };
+        this.videoSearch('')
+    }
+
+    videoSearch (term) {
+        YTsearch({ key: API_KEY, term: term }, (videos) => {
+            this.setState({
+                videos,
+            selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render () {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
